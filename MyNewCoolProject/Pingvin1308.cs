e4b64cf353fe70c9209fddfc37a656a41bdf8d2e
Trufanov Roman <pingvin1308@gmail.com>
2021-03-27 17:55:01 +0300

    add new class

---
+++ MyNewCoolProject/Pingvin1308.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewCoolProject
+{
+    public class Pingvin1308
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
