27199383f77b759adc7d4f5054598ff86a9ff25a
Андрей Федерягин <eniluck@mail.ru>
2021-03-27 19:56:04 +0500

    new state

---
+++ MyNewCoolProject/Users_Eniluck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewCoolProject
+{
+    public class Users_Eniluck
+    {
+        public int ID { get; set; }
+
+        public string Age { get; set; }
+
+    }
+}
