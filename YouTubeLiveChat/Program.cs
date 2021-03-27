61e283759f67d8c1cd1a7cf1492797e5cedc0aa3
blood-mif <vasily_perepelkin@mail.ru>
2021-03-27 18:14:40 +0300

    sorrrr

--- YouTubeLiveChat/Program.cs
+++ YouTubeLiveChat/Program.cs
@@ -5,7 +5,7 @@ namespace YouTubeLiveChat
     class Program
     {
         static void Main(string[] args)
-        {
+        {//455
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
