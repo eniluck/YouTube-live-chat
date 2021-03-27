6cc884b5cd71073d9feafa6148475eca000ce56e
Danyello <ulkiora.panda@gmail.com>
2021-03-27 21:07:38 +0600

    CLR

---
+++ YouTubeLiveChat/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeLiveChat
+{
+    class Class1
+    {
+        class Class2
+        {
+            class Class3
+            {
+                public Class3()
+                {
+
+                }
+            }
+            public Class2()
+            {
+
+            }
+        }
+        public Class1()
+        {
+
+        }
+    }
+}
