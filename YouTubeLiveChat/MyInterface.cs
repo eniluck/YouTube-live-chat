beeae837ab9eada3751b80d4a18d0630bb3e7c86
Danyello <ulkiora.panda@gmail.com>
2021-03-27 20:56:44 +0600

    Very important interface

---
+++ YouTubeLiveChat/MyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeLiveChat
+{
+    public interface MyInterfaceDied5thJuly
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+        Guid Id { get; }
+    }
+}
